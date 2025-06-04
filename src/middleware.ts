34d66788027fc471c708d5917f06f7393b139aa9
ChannabasavaH <channabasavahalemani49@gmail.com>
2025-06-04 11:44:22 +0530

    modified middleware for route protection

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,34 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export const config = {
-    matcher: ['/', '/signup', '/login', '/dashboard'],
+  matcher: [
+    "/",
+    "/signup",
+    "/login",
+    "/dashboard",
+    "/user-profile",
+    "/history",
+    "/favorites",
+  ],
 };
 
 export const middleware = (req: NextRequest) => {
-    const refreshToken = req.cookies.get('refreshToken')?.value;
-    const url = req.nextUrl;
+  const refreshToken = req.cookies.get("refreshToken")?.value;
+  const url = req.nextUrl;
 
-    if (refreshToken && (url.pathname.startsWith('/signup') || url.pathname.startsWith('/login'))) {
-        return NextResponse.redirect(new URL('/dashboard', req.url));
-    }
+  if (
+    refreshToken &&
+    (url.pathname.startsWith("/signup") || url.pathname.startsWith("/login"))
+  ) {
+    return NextResponse.redirect(new URL("/dashboard", req.url));
+  }
 
-    if (!refreshToken && url.pathname.startsWith('/dashboard')) {
-        return NextResponse.redirect(new URL('/login', req.url));
-    }
+  if (
+    (!refreshToken && url.pathname.startsWith("/dashboard")) ||
+    url.pathname.startsWith("/user-profile") ||
+    url.pathname.startsWith("/history") ||
+    url.pathname.startsWith("/favorites")
+  ) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
 };
